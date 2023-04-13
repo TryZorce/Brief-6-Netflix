f802e6dac15548cec27e69a87ef8f998b5d453f4
TryZorce <christopher.moron@gmail.com>
2023-04-13 14:39:22 +0200

    Ajout README et ajout de la variable API_Key

---
+++ README.md
@@ -0,0 +1,3 @@
+Pour faire fonctionner le site : 
+
+Crée un fichier apikey.ts et coller le contenue du fichier apikey.exemple.ts dedans, remplacer la veleur "Key" par votre clé TMBD
--- script.ts
+++ script.ts
@@ -1,11 +1,11 @@
 // import {details} from './pages/details'
 // import {home} from './pages/home'
 // import {search} from './pages/search'
+import { api_key } from "./apikey";
 
-// Code de base
 async function mostpopular() {
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=b7bdd2da6dc32e236c3e6fd84277c32b&language=fr-FR&page=1`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=fr-FR&page=1`);
     const data = await response.json();
     console.log(data.results);
 const scrollDiv = document.querySelector('.scroll');
