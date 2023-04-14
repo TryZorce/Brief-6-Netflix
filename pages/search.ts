34f8513c0572de980d07972cb2bfdc61de7ad6fe
TryZorce <christopher.moron@gmail.com>
2023-04-14 16:30:48 +0200

    update

--- pages/search.ts
+++ pages/search.ts
@@ -1,1 +1,85 @@
+import { api_key } from "../apikey";
 
+export function search() {
+  const openPopupButton = document.getElementById('open-popup') as HTMLElement;
+  const closePopupButton = document.getElementById('close-popup') as HTMLElement;
+  const popup = document.querySelector('.popup') as HTMLElement;
+  const searchInput = document.getElementById('site-search') as HTMLInputElement;
+  const popupTitle = document.getElementById('popup-title') as HTMLElement;
+  const searchResultsElement = document.getElementById('search-results') as HTMLElement;
+
+  openPopupButton?.addEventListener('click', function () {
+    if (popup) {
+      popup.style.display = "flex";
+    }
+  });
+
+  closePopupButton?.addEventListener('click', function () {
+    if (popup) {
+      popup.style.display = "none";
+      resetSearchResults();
+    }
+  });
+
+  searchInput?.addEventListener('input', function (event) {
+    const searchTerm = (event.target as HTMLInputElement).value;
+    if (popupTitle) {
+      popupTitle.innerText = searchTerm;
+    }
+  });
+
+  searchInput?.addEventListener('keydown', async function(event) {
+    if (event.key === 'Enter') { 
+      const searchTerm = searchInput.value.trim(); // retire les espaces en début et en fin de la chaîne
+
+      if (searchTerm.length > 0) {
+        try {
+          const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchTerm}&language=fr-FR&page=1`);
+          const data = await response.json();
+          const searchResults = data.results.slice(0, 10); // limiter les résultats à 10
+
+          // Supprime tous les résultats de recherche précédents
+          resetSearchResults();
+
+          // Ajoute le titre de recherche en haut de la popup
+          const popupTitleElement = document.createElement('h2');
+          popupTitleElement.innerText = searchTerm;
+          searchResultsElement?.appendChild(popupTitleElement);
+
+          // Crée un élément HTML pour chaque résultat de recherche et l'ajoute à la liste de résultats
+          searchResults.forEach((result) => {
+            const title = result.title;
+            const posterPath = result.poster_path;
+            const listItem = document.createElement('li');
+          
+            if (posterPath) {
+              const img = document.createElement('img');
+              img.src = `https://image.tmdb.org/t/p/w300/${posterPath}`;
+              listItem.appendChild(img);
+            }
+          
+            const titleElement = document.createElement('h3');
+            titleElement.innerText = title;
+            listItem.appendChild(titleElement);
+          
+            searchResultsElement?.appendChild(listItem);
+          });
+          
+
+          if (popup) {
+            popup.style.display = "flex";
+          }
+
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  });
+
+  function resetSearchResults() {
+    while (searchResultsElement.firstChild) {
+      searchResultsElement.removeChild(searchResultsElement.firstChild);
+    }
+  }
+}
--- script.ts
+++ script.ts
@@ -1,18 +1,40 @@
 // import {details} from './pages/details'
 // import {home} from './pages/home'
-// import {search} from './pages/search'
+import {search} from './pages/search'
 import { api_key } from "./apikey";
+
+// Fonction qui récupère 
+search();
+
+// Récuperer film + populaire
+
+async function mostpopular_banner() {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=fr-FR&page=1`);
+    const data = await response.json();
+    const firstMovie = data.results[0]; 
+    const netflix_banner = document.querySelector('.netflix_banner')
+console.log(firstMovie);
+
+    const poster = document.createElement('img');
+    poster.setAttribute('src', 'https://image.tmdb.org/t/p/w780' + firstMovie.backdrop_path); 
+    netflix_banner?.appendChild(poster);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+mostpopular_banner();
+
 
 async function mostpopular() {
   try {
     const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=fr-FR&page=1`);
     const data = await response.json();
-    console.log(data.results);
     const scrollDiv = document.querySelector('.scroll');
 
     const popularMovies = data.results;
     popularMovies.forEach(movie => {
-      console.log(movie.poster_path);
 
       const domImg = document.createElement('img');
       domImg.setAttribute('src', 'https://image.tmdb.org/t/p/w185' + movie.poster_path);
@@ -27,16 +49,16 @@ async function mostpopular() {
 
 mostpopular();
 
+// Récuperer film Mieux classée
+
 async function toprated() {
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b7bdd2da6dc32e236c3e6fd84277c32b&language=fr-FR&page=1`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=fr-FR&page=1`);
     const data = await response.json();
-    console.log(data.results);
     const scrollDiv = document.getElementById('toprated');
 
     const popularMovies = data.results;
     popularMovies.forEach(movie => {
-      console.log(movie.poster_path);
 
       const domImg = document.createElement('img');
       domImg.setAttribute('src', 'https://image.tmdb.org/t/p/w185' + movie.poster_path);
@@ -51,16 +73,16 @@ async function toprated() {
 
 toprated();
 
+// Récuperer film prochainement
+
 async function upcoming() {
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=b7bdd2da6dc32e236c3e6fd84277c32b&language=en-US&page=1`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`);
     const data = await response.json();
-    console.log(data.results);
     const scrollDiv = document.getElementById('latest');
 
     const popularMovies = data.results;
     popularMovies.forEach(movie => {
-      console.log(movie.poster_path);
 
       const domImg = document.createElement('img');
       domImg.setAttribute('src', 'https://image.tmdb.org/t/p/w185' + movie.poster_path);
@@ -74,3 +96,6 @@ async function upcoming() {
 }
 
 upcoming();
+
+  
+  
